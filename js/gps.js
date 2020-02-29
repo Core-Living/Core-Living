cc9c623bdf20c54911178019956ff52eb7e4d35e
Daniel Johnston <daniel.august.j@gmail.com>
2020-02-29 10:07:38 -0600

    commented out distance equation
    testing gps and had to comment out distance equation

--- js/gps.js
+++ js/gps.js
@@ -11,19 +11,19 @@ function GPS()
 			{
 				if(this.currentLat != null && this.currentLong != null)
 				{
-					var R = 6371e3; // metres
-					var φ1 = this.currentLat.toRadians();
-					var φ2 = position.coords.latitude.toRadians();
-					var Δφ = (position.coords.latitude - this.currentLat).toRadians();
-					var Δλ = (position.coords.longitude - this.currentLong).toRadians();
+					//var R = 6371e3; // metres
+					//var φ1 = this.currentLat.toRadians();
+					//var φ2 = position.coords.latitude.toRadians();
+					//var Δφ = (position.coords.latitude - this.currentLat).toRadians();
+					//var Δλ = (position.coords.longitude - this.currentLong).toRadians();
 
-					var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-							Math.cos(φ1) * Math.cos(φ2) *
-							Math.sin(Δλ/2) * Math.sin(Δλ/2);
-					var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+					//var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+							//Math.cos(φ1) * Math.cos(φ2) *
+							//Math.sin(Δλ/2) * Math.sin(Δλ/2);
+					//var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-					var d = R * c;
-					alert(d);
+					//var d = R * c;
+					//alert(d);
 				}
 				document.querySelector('#oldlong').textContent = this.currentLong;
 				document.querySelector('#oldlat').textContent = this.currentLat;
