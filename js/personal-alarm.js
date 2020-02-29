c58015490aa057a8415133a74dc65609a57352c9
noahrr2 <noahrr2@illinois.edu>
2020-02-29 12:48:30 -0600

    feat: personal alarm functionality implemented

--- js/personal-alarm.js
+++ js/personal-alarm.js
@@ -1,16 +1,42 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var audCon = new AudioContext();
-var osci = audCon.createOscillator();
+// var osci = audCon.createOscillator();
+// HTML is <input id="volume" type="range" min="0" max="1" step="0.1" value="0.5"/>
 var gain = audCon.createGain();
+gain.connect(audCon.destination)
+
+function increaseVolume() {
+    document.getElementById('volume').addEventListener('change', function() {
+        var currentVol = this.value;
+        gain.gain.value = currentVol;
+        console.log("Current Volume: ", currentVol);
+    })
+}
 
 function alarm() {
-    osci.frequency.value = 780
-    osci.connect(gain)
-    gain.connect(audCon.destination)
-    osci.start(0)
+    // osci.frequency.value = 780
+    // osci.connect(gain)
+    var request = new XMLHttpRequest();
+    var audFile = "alarm.mp3";
+    request.open('GET', audFile, true);
+    request.responseType = 'arraybuffer';
+
+    request.onload = function() {
+        audCon.decodeAudioData(request.response, function(buffer) {
+            store = buffer;
+            var src = audCon.createBufferSource();
+            src.buffer = buffer;
+            src.connect(gain);
+            src.start(0)
+        });
+    };
+    request.send();
+    increaseVolume();
+    // osci.start(0)
 }
 
 function stopAlarm() {
     gain.gain.exponentialRampToValueAtTime(
-        0.0000001, audCon.currentTime + 0.04
+        0.00001, audCon.currentTime + 0.04
     )
 }
